1f1944ef8ffeb4d43a3f449fc9dc834ccda8a5ef
Matheus Lins <matheuslins9@gmail.com>
2020-10-01 22:35:04 -0300

    Add custom settings

--- burberryCrawl/spiders/burberry/burberry.py
+++ burberryCrawl/spiders/burberry/burberry.py
@@ -7,6 +7,9 @@ class BurberrySpider(scrapy.Spider):
     name = 'burberry'
     allowed_domains = ['us.burberry.com']
     start_urls = ['http://us.burberry.com/']
+    custom_settings = {
+        'DOWNLOAD_DELAY': 0.5,
+    }
 
     def parse(self, response):
         for item in extract_clothes(response) or []:
