9f0f677dc4dd186ed48826e1798a08a0fdf7736e
Matheus Lins <matheuslins9@gmail.com>
2020-10-04 15:37:06 -0300

    Fix product link for some cases

--- burberryCrawl/spiders/burberry/constants/consult.py
+++ burberryCrawl/spiders/burberry/constants/consult.py
@@ -3,6 +3,9 @@ START_URL = 'http://us.burberry.com'
 
 CONSULT_XPATHS = {
     'CATEGORIES': '//a[@class="css-g38vah e1268c320"]//@href',
-    'PRODUCT_LINKS': '//a[@class="product-card__link"]//@href'
+    'PRODUCT_LINKS': '//a[@class="product-card__link"]//@href | '
+                     '//div[contains(@class, "cell-asset")]//a[contains(@class, "product-internal-link")]/@href',
+    'SHOW_MORE_BUTTON': '//button[@aria-label="View more products"]',
+    'SHOW_MORE_LINK': '//div[contains(@class, "view-more-button")]'
 }
 
